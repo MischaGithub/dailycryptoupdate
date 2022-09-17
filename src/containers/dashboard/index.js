c4bd2576154c0c016313df422b22d35e19941fb2
MischaGithub <mischa@bnry.digital>
2022-09-17 18:54:10 +0200

    dashboard

--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import CryptoItem from '../../components/crypto-item';
+import fetchInititalData from '../../components/utils/fetchInititalData';
 import styles from './dashboard.module.scss';
 
 const Dashboard = () => {
+  const { response } = fetchInititalData(
+    'coins/markets?vs_currency=zar&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+  );
+
   return (
     <div className={styles.container}>
       <div className={styles.headingContainer}>
@@ -27,16 +32,18 @@ const Dashboard = () => {
             </li>
 
             <li className={styles.headerChart}>
-              <span className={styles.headerTitle}>Last 7 days</span>
+              <span className={styles.headerTitle}>24h</span>
             </li>
           </ul>
         </div>
         <ul className={styles.cryptoList}>
-          <CryptoItem />
+          {response?.map((coinItem) => (
+            <li key={coinItem.id} className={styles.coinBlock}>
+              <CryptoItem coinItem={coinItem} />
+            </li>
+          ))}
         </ul>
       </section>
-
-      {/* <a href='/books'>Dashboard</a> */}
     </div>
   );
 };
